8e575a42708b831aabf5272bb23473bad77c3779
A <alyasakhnini2021@gmail.com>
2025-05-02 14:01:06 +0300

    Added cutscene 1 subtitles and audio

---
+++ BlackwoodsCurse/Assets/Scripts/DialogSignalTrigger.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Fungus;
+
+public class DialogSignalTrigger : MonoBehaviour
+{
+    public Flowchart flowchart;
+
+    public void TriggerApproachLine()
+    {
+        flowchart.ExecuteBlock("ApproachMansionLine");
+    }
+
+    public void TriggerAtMansionLine()
+    {
+        flowchart.ExecuteBlock("AtMansionLine");
+    }
+}
