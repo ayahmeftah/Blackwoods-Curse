4c54e99c436f167e9ddfa97b07e7da591feb0959
Ayah Meftah <138591919+ayahmeftah@users.noreply.github.com>
2025-05-12 06:10:11 +0300

    started piano mode and added stars and zone indicators to piano mode

--- BlackwoodsCurse/Assets/PianoInteractionTrigger.cs
+++ BlackwoodsCurse/Assets/PianoInteractionTrigger.cs
@@ -1,18 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PianoInteractionTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public HUD hud;
+    public GameObject piano3DModel;
+    public GameObject pianoOverlay; // Canvas with “Back (B)”
+    public Camera mainCamera;
+    public Camera pianoCamera;
+    public GameObject playerController;
 
-    // Update is called once per frame
+    public GameObject inventoryUI;
+
+
+    private bool isPlayerNear = false;
+    private bool inPianoMode = false;
+
     void Update()
     {
-        
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.P) && !inPianoMode)
+        {
+            EnterPianoMode();
+        }
+
+        if (inPianoMode && Input.GetKeyDown(KeyCode.X))
+        {
+            ExitPianoMode();
+        }
+    }
+
+    void EnterPianoMode()
+    {
+        if (inventoryUI != null)
+            inventoryUI.SetActive(false);
+
+
+        inPianoMode = true;
+        hud.HideMessage();
+
+        mainCamera.enabled = false;
+        pianoCamera.enabled = true;
+
+        piano3DModel.SetActive(true);
+        pianoOverlay.SetActive(true);
+        if (playerController != null)
+            playerController.SetActive(false);
+    }
+
+    void ExitPianoMode()
+    {
+        if (inventoryUI != null)
+            inventoryUI.SetActive(true);
+
+
+        inPianoMode = false;
+
+        mainCamera.enabled = true;
+        pianoCamera.enabled = false;
+
+        piano3DModel.SetActive(false);
+        pianoOverlay.SetActive(false);
+        if (playerController != null)
+            playerController.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+            hud.txt.text = "Enter Piano Mode P";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+            hud.HideMessage();
+        }
     }
 }
+
